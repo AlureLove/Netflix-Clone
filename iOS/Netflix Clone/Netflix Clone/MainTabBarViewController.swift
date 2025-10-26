a323a94ae0254ad1a45fd196edab883df7b0c6d5
Jethro <qhlrx1@gmail.com>
2025-10-26 22:13:46 +0900

    feat: add iOS project

---
+++ iOS/Netflix Clone/Netflix Clone/MainTabBarViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix Clone
+//
+//  Created by Jethro Liu on 2025/10/26.
+//
+
+import UIKit
+
+class MainTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemYellow
+    }
+
+
+}
+
