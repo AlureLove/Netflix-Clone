754a4538007e4d5f75de56b11aa330f34fd90b56
Jethro <qhlrx1@gmail.com>
2025-11-19 23:02:28 +0900

    feat: upcoming

--- iOS/Netflix Clone/Netflix Clone/Controllers/UpcomingViewController.swift
+++ iOS/Netflix Clone/Netflix Clone/Controllers/UpcomingViewController.swift
@@ -8,10 +8,68 @@
 import UIKit
 
 class UpcomingViewController: UIViewController {
+    
+    private var titles: [Title] = [Title]()
+    
+    private let upcomingTable: UITableView = {
+        let tableView = UITableView()
+        tableView.register(TitleTableViewCell.self, forCellReuseIdentifier: TitleTableViewCell.identifier)
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .systemBackground
+        title = "Upcoming"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationItem.largeTitleDisplayMode = .always
+        
+        view.addSubview(upcomingTable)
+        upcomingTable.dataSource = self
+        upcomingTable.delegate = self
+        
+        fetchUpcoming()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        upcomingTable.frame = view.bounds
+    }
+    
+    private func fetchUpcoming() {
+        Task { [weak self] in
+            do {
+                let titles = try await APICaller.shared.getUpcomingMovies()
+                self?.titles = titles
+                await MainActor.run {
+                    self?.upcomingTable.reloadData()
+                }
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
+
+extension UpcomingViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        titles.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: TitleTableViewCell.identifier, for: indexPath) as? TitleTableViewCell else {
+            return UITableViewCell()
+        }
+        
+        let title = titles[indexPath.row]
+        cell.configure(with: TitleViewModel(titleName: title.original_title ?? title.original_name ?? "unknown", posterURL: title.poster_path ?? "https://picsum.photos/200"))
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        140
     }
 }
---
+++ iOS/Netflix Clone/Netflix Clone/ViewModels/TitleViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TitleViewModel.swift
+//  Netflix Clone
+//
+//  Created by Jethro Liu on 2025/11/19.
+//
+
+import Foundation
+
+struct TitleViewModel {
+    let titleName: String
+    let posterURL: String
+}
